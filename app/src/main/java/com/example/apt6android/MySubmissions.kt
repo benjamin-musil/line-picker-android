1a6d7abb607e30e8f7a4cd6ee45346fa915d571f
sanjaikanth <sanjaikanth@gmail.com>
2019-10-20 15:53:57 -0500

    Andoid UI COmponents

---
+++ app/src/main/java/com/example/apt6android/MySubmissions.kt
@@ -0,0 +1,12 @@
+package com.example.apt6android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MySubmissions : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_submissions)
+    }
+}
