f8800ef3886e08addcdfae1c711f8c0ddbc38b3c
sanjaikanth <sanjaikanth@gmail.com>
2019-10-21 11:10:44 -0500

    Adding My Submittion

--- app/src/main/java/com/example/apt6android/MySubmissions.kt
+++ app/src/main/java/com/example/apt6android/MySubmissions.kt
@@ -2,11 +2,16 @@ package com.example.apt6android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MySubmissions : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //findViewById<ListView>(R.id.WaitTimeList)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_submissions)
+
+
     }
 }
