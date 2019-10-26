fb07ca127d92de4091ca4bd2ded9dea60d560aee
Nathan Wiatrek <nwiatrek@gmail.com>
2019-10-26 11:35:48 -0500

    adding what sanjai did

---
+++ app/src/main/java/com/example/apt_line_picker_app/View/AddRestaurant.kt
@@ -0,0 +1,13 @@
+package com.example.apt_line_picker_app.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apt_line_picker_app.R
+
+class AddRestaurant : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_restaurant)
+    }
+}
