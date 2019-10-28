a198b96f5cfc89e1581672080dffcf1fcc5fee14
sanjaikanth <sanjaikanth@gmail.com>
2019-10-28 16:13:21 -0500

    User Settings

---
+++ app/src/main/java/com/example/apt_line_picker_app/Model/User.kt
@@ -0,0 +1,10 @@
+package com.example.apt_line_picker_app.Model
+
+class User {
+    var name:String = ""
+    var email:String=""
+    var favorite_food:String=""
+    var user_id:String=""
+    var role:String=""
+
+}
